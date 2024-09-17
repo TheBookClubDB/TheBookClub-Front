82443b461f13e73afbb625815b3c6f3eae57b95f
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-17 17:47:20 -0300

    feat: Cria Página Form Autor

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/App.tsx'
 import './styles/main.css'
+import FormAutor from './pages/FormAutor'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FormAutor />
   </StrictMode>,
 )
