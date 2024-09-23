544b9739e72999c9b568d51254a915693e903073
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-09-23 14:02:18 -0300

    Corrigindo configuração do test

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -1,9 +1,9 @@
-import { describe, expect, it, test } from 'vitest'
+import { describe, test} from 'vitest'
 import App from './App'
 import { render } from '@testing-library/react'
 
 describe('Realiza teste da pagina app', ()=>{
-    it('Valida se a pagina APP eta sendo renderizada', ()=>{
+    test('Valida se a pagina APP eta sendo renderizada', ()=>{
         render(<App/>);
     })
 
