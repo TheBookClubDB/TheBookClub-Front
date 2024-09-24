66cae758faeab31e5636c664f14deebda79846ad
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-24 13:58:32 -0300

    refactor: Fix Lint Errors

--- src/pages/FormAutor/index.tsx
+++ src/pages/FormAutor/index.tsx
@@ -1,13 +1,13 @@
-import { Box, Container } from "@mui/material"
-import Sidebar from "../../components/Sidebar"
+import { Box, Container } from '@mui/material'
+import Sidebar from '../../components/Sidebar'
 
 const FormAutor = () => {
-    return (
-        <Box sx={{ display: "flex"}}>
-            <Sidebar item="autor"/>
-            <Container/>
-        </Box>
-    )
+  return (
+    <Box sx={{ display: 'flex'}}>
+      <Sidebar item="autor"/>
+      <Container/>
+    </Box>
+  )
 }
 
 export default FormAutor;
