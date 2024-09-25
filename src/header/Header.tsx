73a304284f94a3486fa40febc5c1fedb9cee7f9a
Tomsilva69 <evertondosantos69@gmail.com>
2024-09-25 16:55:44 -0300

    feat: Implementa o Header

---
+++ src/header/Header.tsx
@@ -0,0 +1,28 @@
+import styles from './Header.module.css'
+import Logo from '../assets/Bookclube-branco.png'
+import Icon from '../assets/user-iconn.png'
+
+function Header(){
+
+    return(
+        <header>
+            <nav className={styles.navbar}> 
+
+                <div className={styles.logo}>
+                    <img src={Logo} alt="Logo do site"/>
+                </div>
+        
+                <ul className={styles.navlinks}>
+                    <li><a href="#"><strong>O que é Bookclub?</strong></a></li>
+                    <li><a href="#"><strong>Contato</strong></a></li>
+                </ul>
+                <div className={styles.account}>
+                    <img src={Icon} alt="foto da conta"/>
+                    <a href="#"><strong>Adiminstrtador</strong></a>
+                </div>
+
+            </nav>
+        </header>
+    )
+}
+export default Header;
--- src/pages/FormAutor/index.tsx
+++ src/pages/FormAutor/index.tsx
@@ -1,12 +1,16 @@
 import { Box, Container } from '@mui/material'
 import Sidebar from '../../components/Sidebar'
+import Header from '../../header/Header';
 
 const FormAutor = () => {
   return (
+    <>
+    <Header/>
     <Box sx={{ display: 'flex'}}>
       <Sidebar item="autor"/>
       <Container/>
     </Box>
+    </>
   )
 }
 
