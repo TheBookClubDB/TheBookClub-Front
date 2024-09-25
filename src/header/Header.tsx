b73f66a0a701e7706e6f27f5d5322d079058c43e
Tomsilva69 <evertondosantos69@gmail.com>
2024-09-25 17:03:07 -0300

    fix: Conserta os erros da pipeline

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -4,25 +4,25 @@ import Icon from '../assets/user-iconn.png'
 
 function Header(){
 
-    return(
-        <header>
-            <nav className={styles.navbar}> 
+  return(
+    <header>
+      <nav className={styles.navbar}> 
 
-                <div className={styles.logo}>
-                    <img src={Logo} alt="Logo do site"/>
-                </div>
+        <div className={styles.logo}>
+          <img src={Logo} alt="Logo do site"/>
+        </div>
         
-                <ul className={styles.navlinks}>
-                    <li><a href="#"><strong>O que é Bookclub?</strong></a></li>
-                    <li><a href="#"><strong>Contato</strong></a></li>
-                </ul>
-                <div className={styles.account}>
-                    <img src={Icon} alt="foto da conta"/>
-                    <a href="#"><strong>Adiminstrtador</strong></a>
-                </div>
+        <ul className={styles.navlinks}>
+          <li><a href="#"><strong>O que é Bookclub?</strong></a></li>
+          <li><a href="#"><strong>Contato</strong></a></li>
+        </ul>
+        <div className={styles.account}>
+          <img src={Icon} alt="foto da conta"/>
+          <a href="#"><strong>Adiminstrtador</strong></a>
+        </div>
 
-            </nav>
-        </header>
-    )
+      </nav>
+    </header>
+  )
 }
 export default Header;
--- src/pages/FormAutor/index.tsx
+++ src/pages/FormAutor/index.tsx
@@ -5,11 +5,11 @@ import Header from '../../header/Header';
 const FormAutor = () => {
   return (
     <>
-    <Header/>
-    <Box sx={{ display: 'flex'}}>
-      <Sidebar item="autor"/>
-      <Container/>
-    </Box>
+      <Header/>
+      <Box sx={{ display: 'flex'}}>
+        <Sidebar item="autor"/>
+        <Container/>
+      </Box>
     </>
   )
 }
