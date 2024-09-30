af15ec03a26741f6b7511b1a404b2cf6a907c712
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-30 11:14:18 -0300

    fix: Conserta Importação

--- src/pages/FormAutor/index.tsx
+++ src/pages/FormAutor/index.tsx
@@ -1,10 +1,12 @@
 import { Box, Container } from '@mui/material'
 import Sidebar from '../../components/Sidebar'
 import Botao from '../../components/botao/Botao';
+import Header from '../../components/Header';
 
 const FormAutor = () => {
   return (
     <Box sx={{ display: 'flex'}}>
+      <Header/>
       <Sidebar item="autor"/>
       <Container>
         <Botao texto="cancelar" variante="outlined" onClick={() => {}} />
