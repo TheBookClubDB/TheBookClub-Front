2b75da8c372c752f1b82266139b372fc1acaf2a6
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-30 07:34:21 -0300

    fix: Adiciona imports e conserta teste

--- src/components/botao/Botao.test.tsx
+++ src/components/botao/Botao.test.tsx
@@ -1,23 +1,19 @@
-import { render, screen, fireEvent } from '@testing-library/React'
-import Button from './Button'
+import { render, screen, fireEvent } from '@testing-library/react'
+import {describe, expect, test, vi} from 'vitest'
+import Button from './Botao'
  
-describe('Button Component', () => {
-  test('renders with the correct label', () => {
-    const label = 'Click me'
-    render(<Button label={label} onClick={() => {}} />)
- 
-    // Verifica se o botão está no documento com o texto correto
+describe('Testa o Componente de Botão', () => {
+  const label = 'Click me'
+  const onClickMock = vi.fn()
+  test('Renderiza o botão', () => {
+    render(<Button texto={label} onClick={() => {}} />)
   })
  
-  test('calls onClick handler when clicked', () => {
-    const onClickMock = jest.fn() // Cria uma função mock
-    const label = 'Click me'
-    render(<Button label={label} onClick={onClickMock} />)
+  test('Testa o clique do botão', () => {
+    render(<Button texto={label} onClick={onClickMock} />)
  
-    // Simula um clique no botão
     fireEvent.click(screen.getByText(label))
- 
-    // Verifica se a função mock foi chamada
+
     expect(onClickMock).toHaveBeenCalledTimes(1)
   })
 })
