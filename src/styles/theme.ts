ff5a95b35f1f286fb5d46e9c477b5984034d80a4
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-10-04 16:01:28 -0300

    fix: Erros de Lint

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,12 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material';
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#850F8D",
-            light: "#fef3ff",
-        }
+  palette: {
+    primary: {
+      main: '#850F8D',
+      light: '#fef3ff',
     }
+  }
 })
 
 export default theme;
