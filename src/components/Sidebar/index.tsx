25547b415cbe0fbd4c6a18a40f80cdcfd090c7e6
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-10-09 12:27:54 -0300

    fix: Reestrutura Sidebar e Rotas

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,77 +1,156 @@
-import { Box, Divider, List, ListItem, TextField } from '@mui/material';
-import LogoutIcon from '@mui/icons-material/Logout';
-import SettingsIcon from '@mui/icons-material/Settings';
+import { ConfigProvider, Menu, MenuProps } from "antd";
 import styles from './sidebar.module.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { NavLink } from "react-router-dom";
 
-interface itemClicado {
-    item: string
+interface paginaAtual {
+    pagina: string
 }
 
-const Sidebar = (props: itemClicado) => {
+type MenuItem = Required<MenuProps>['items'][number];
 
-  const lidarItemSelecionado = (itemSelecionado: string) => {
-    let componente = null;
-    switch (itemSelecionado) {
-    case 'aluguel':
-      componente = document.querySelector('#aluguelItem')
-      break;
-    case 'geren':
-      componente = document.querySelector('#gerenItem')
-      break;
-    case 'autor':
-      componente = document.querySelector('#autorGerenItem')
-      break;
-    case 'locat':
-      componente = document.querySelector('#locatGerenItem')
-      break;
-    case 'livros':
-      componente = document.querySelector('#livrosGerenItem')
-      break;
-    default:
-      break;
-    }
-    if (componente) {
-      (componente as HTMLElement).style.backgroundColor = 'rgba(33, 147, 243, 0.1)'
+const items: MenuItem[] = [
+  {
+    key: 'sub1',
+    label: 'Aluguéis',
+    children: [
+      { key: '1', label: (
+        <NavLink to={'/devolver'}>
+          Devolver 
+        </NavLink>
+        )},
+      { key: '2', label: (
+        <NavLink to={'/alugar'}>
+          Alugar
+        </NavLink>
+        )},
+      { key: '3', label: (
+        <NavLink to={'/historico'}>
+          Ver Histórico 
+        </NavLink>
+        )},
+    ],
+  },
+  {
+    key: 'sub2',
+    label: 'Cadastros',
+    children: [
+      { key: '4', label: (
+        <NavLink to={'/criar-autor'}>
+          Autores 
+        </NavLink>
+        )},
+      { key: '5', label: (
+        <NavLink to={'/criar-locatario'}>
+          Locatários
+        </NavLink>
+        )},
+      { key: '6', label: (
+        <NavLink to={'/criar-livro'}>
+          Livros 
+        </NavLink>
+        )},
+    ],
+  },
+  {
+    key: 'sub3',
+    label: 'Gerenciar',
+    children: [
+      { key: '7', label: (
+        <NavLink to={'/geren-autor'}>
+          Autores 
+        </NavLink>
+        )},
+      { key: '8', label: (
+        <NavLink to={'/geren-locat'}>
+          Autores 
+        </NavLink>
+        )},
+      { key: '9', label: (
+        <NavLink to={'/geren-autor'}>
+          Livros 
+        </NavLink>
+        )}
+    ],
+  }
+];
+
+const Sidebar = ({pagina}: paginaAtual) => {
+  const [selectedTab, setSelectedTab] = useState("0")
+  const [openKeys, setOpenKeys] = useState<string[]>([])
+
+  const paginaSelecionada = (pagina: string) => {
+    switch (pagina) {
+      case 'aDevol':
+        setOpenKeys(["sub1"])
+        setSelectedTab("1");
+        break;
+      case 'aAlug':
+        setOpenKeys(["sub1"])
+        setSelectedTab("2");
+        break;
+      case 'aHist':
+        setOpenKeys(["sub1"])
+        setSelectedTab("3");
+        break;
+      case 'cAutor':
+        setOpenKeys(["sub2"])
+        setSelectedTab("4");
+        break;
+      case 'cLocat':
+        setOpenKeys(["sub2"])
+        setSelectedTab("5");
+        break;
+      case 'cLivro':
+        setOpenKeys(["sub2"])
+        setSelectedTab("6");
+        break;
+      case 'gAutor':
+        setOpenKeys(["sub3"])
+        setSelectedTab("7");
+        break;
+      case 'gLocat':
+        setOpenKeys(["sub3"])
+        setSelectedTab("8");
+        break;
+      case 'gLivro':
+        setOpenKeys(["sub3"])
+        setSelectedTab("9");
+        break;
+      default:
+        setOpenKeys([])
+        setSelectedTab("0");
+        break;
     }
   }
 
   useEffect(() => {
-    lidarItemSelecionado(props.item)
-  }, [props.item])
+    paginaSelecionada(pagina)
+  }, [pagina])
 
   return(
-    <Box display='flex'>
-      <Box className={styles.sidebarContainer}> 
-        <TextField 
-          className={styles.campoBusca} 
-          placeholder="Pesquisar"
-          size="small"
-        />
-        <List className={styles.lista}>
-          <ListItem id="aluguelItem">
-            <LogoutIcon className={styles.iconeLista}/>
-                        Aluguel
-          </ListItem>
-          <Divider/>
-          <ListItem id="gerenItem">
-            <SettingsIcon className={styles.iconeLista}/>
-                        Gerenciador
-          </ListItem>
-          <Divider/>
-          <ListItem id="autorGerenItem">
-                        Gerenciar Autores
-          </ListItem>
-          <ListItem id="locatGerenItem">
-                        Gerenciar Locatários
-          </ListItem>
-          <ListItem id="livrosGerenItem">
-                        Gerenciar Livros
-          </ListItem>
-        </List>
-      </Box>
-      <Divider orientation="vertical" variant="fullWidth" sx={{ color: 'red'}}/>
-    </Box>
+    <ConfigProvider
+      theme={{
+        components: {
+          Menu: {
+            colorTextSecondary: "#ff3",
+            itemSelectedBg: "#850F8D",
+            itemSelectedColor: "#fff",
+            itemColor: "#000",
+            itemBg: "#fef3ff",        
+          }
+        }
+      }}
+    >
+      <Menu
+        className={styles.sidebarContainer}
+        onOpenChange={setOpenKeys}
+        openKeys={openKeys}
+        selectedKeys={[selectedTab]}
+        mode="inline"
+        items={items}
+      />
+    </ConfigProvider>
   )
 }
 
