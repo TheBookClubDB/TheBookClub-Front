627789a11872cbe63816d5778b663f5c27fff882
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-10-09 12:48:10 -0300

    fix: Conserta Erros de Lint

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
-import { ConfigProvider, Menu, MenuProps } from "antd";
+import { ConfigProvider, Menu, MenuProps } from 'antd';
 import styles from './sidebar.module.css'
 import { useEffect, useState } from 'react';
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 interface paginaAtual {
     pagina: string
@@ -18,17 +18,17 @@ const items: MenuItem[] = [
         <NavLink to={'/devolver'}>
           Devolver 
         </NavLink>
-        )},
+      )},
       { key: '2', label: (
         <NavLink to={'/alugar'}>
           Alugar
         </NavLink>
-        )},
+      )},
       { key: '3', label: (
         <NavLink to={'/historico'}>
           Ver Histórico 
         </NavLink>
-        )},
+      )},
     ],
   },
   {
@@ -39,17 +39,17 @@ const items: MenuItem[] = [
         <NavLink to={'/criar-autor'}>
           Autores 
         </NavLink>
-        )},
+      )},
       { key: '5', label: (
         <NavLink to={'/criar-locatario'}>
           Locatários
         </NavLink>
-        )},
+      )},
       { key: '6', label: (
         <NavLink to={'/criar-livro'}>
           Livros 
         </NavLink>
-        )},
+      )},
     ],
   },
   {
@@ -60,67 +60,67 @@ const items: MenuItem[] = [
         <NavLink to={'/geren-autor'}>
           Autores 
         </NavLink>
-        )},
+      )},
       { key: '8', label: (
         <NavLink to={'/geren-locat'}>
           Autores 
         </NavLink>
-        )},
+      )},
       { key: '9', label: (
         <NavLink to={'/geren-autor'}>
           Livros 
         </NavLink>
-        )}
+      )}
     ],
   }
 ];
 
 const Sidebar = ({pagina}: paginaAtual) => {
-  const [selectedTab, setSelectedTab] = useState("0")
+  const [selectedTab, setSelectedTab] = useState('0')
   const [openKeys, setOpenKeys] = useState<string[]>([])
 
   const paginaSelecionada = (pagina: string) => {
     switch (pagina) {
-      case 'aDevol':
-        setOpenKeys(["sub1"])
-        setSelectedTab("1");
-        break;
-      case 'aAlug':
-        setOpenKeys(["sub1"])
-        setSelectedTab("2");
-        break;
-      case 'aHist':
-        setOpenKeys(["sub1"])
-        setSelectedTab("3");
-        break;
-      case 'cAutor':
-        setOpenKeys(["sub2"])
-        setSelectedTab("4");
-        break;
-      case 'cLocat':
-        setOpenKeys(["sub2"])
-        setSelectedTab("5");
-        break;
-      case 'cLivro':
-        setOpenKeys(["sub2"])
-        setSelectedTab("6");
-        break;
-      case 'gAutor':
-        setOpenKeys(["sub3"])
-        setSelectedTab("7");
-        break;
-      case 'gLocat':
-        setOpenKeys(["sub3"])
-        setSelectedTab("8");
-        break;
-      case 'gLivro':
-        setOpenKeys(["sub3"])
-        setSelectedTab("9");
-        break;
-      default:
-        setOpenKeys([])
-        setSelectedTab("0");
-        break;
+    case 'aDevol':
+      setOpenKeys(['sub1'])
+      setSelectedTab('1');
+      break;
+    case 'aAlug':
+      setOpenKeys(['sub1'])
+      setSelectedTab('2');
+      break;
+    case 'aHist':
+      setOpenKeys(['sub1'])
+      setSelectedTab('3');
+      break;
+    case 'cAutor':
+      setOpenKeys(['sub2'])
+      setSelectedTab('4');
+      break;
+    case 'cLocat':
+      setOpenKeys(['sub2'])
+      setSelectedTab('5');
+      break;
+    case 'cLivro':
+      setOpenKeys(['sub2'])
+      setSelectedTab('6');
+      break;
+    case 'gAutor':
+      setOpenKeys(['sub3'])
+      setSelectedTab('7');
+      break;
+    case 'gLocat':
+      setOpenKeys(['sub3'])
+      setSelectedTab('8');
+      break;
+    case 'gLivro':
+      setOpenKeys(['sub3'])
+      setSelectedTab('9');
+      break;
+    default:
+      setOpenKeys([])
+      setSelectedTab('0');
+      break;
     }
   }
 
@@ -133,11 +133,11 @@ const Sidebar = ({pagina}: paginaAtual) => {
       theme={{
         components: {
           Menu: {
-            colorTextSecondary: "#ff3",
-            itemSelectedBg: "#850F8D",
-            itemSelectedColor: "#fff",
-            itemColor: "#000",
-            itemBg: "#fef3ff",        
+            colorTextSecondary: '#ff3',
+            itemSelectedBg: '#850F8D',
+            itemSelectedColor: '#fff',
+            itemColor: '#000',
+            itemBg: '#fef3ff',        
           }
         }
       }}
