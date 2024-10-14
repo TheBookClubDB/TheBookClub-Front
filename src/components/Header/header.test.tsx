d4c9b879b54241dfafc5a234c20c23570de0279e
Tomsilva69 <evertondosantos69@gmail.com>
2024-10-14 14:13:20 -0300

    fix: Correção de aspas simples.

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -5,31 +5,31 @@ import { BrowserRouter } from 'react-router-dom';
 
 describe('Testa o componente da Header', () => {
 
-    test('Deve verificar a renderização', () => {
-        render(
-            <BrowserRouter>
-                <Header />
-            </BrowserRouter>)
-        const logo = screen.getByRole("Logo");
-        const avatar = screen.getByRole("Avatar");
+  test('Deve verificar a renderização', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>)
+    const logo = screen.getByRole('Logo');
+    const avatar = screen.getByRole('Avatar');
 
-        const componentes = [logo, avatar];
+    const componentes = [logo, avatar];
 
-        componentes.forEach((componente => {
-            expect(componente).toBeVisible()
-        }))
-    })
+    componentes.forEach((componente => {
+      expect(componente).toBeVisible()
+    }))
+  })
 
-    test('Testa Redirecionamento da Home', () => {
-        render(
-            <BrowserRouter>
-                <Header />
-            </BrowserRouter>)
-        const logo = screen.getByRole('Logo')
-        expect(logo).toBeVisible()
+  test('Testa Redirecionamento da Home', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>)
+    const logo = screen.getByRole('Logo')
+    expect(logo).toBeVisible()
 
-        logo.click();
-        expect(window.location.pathname).toBe('/')
-    })
+    logo.click();
+    expect(window.location.pathname).toBe('/')
+  })
  
 });
