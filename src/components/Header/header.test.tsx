e5d44044c1610ae2e3db003a501b4af915c5963f
Tomsilva69 <152219212+Tomsilva69@users.noreply.github.com>
2024-10-16 10:37:10 -0300

    fix/Header (#11)
    * fix: Faz a refaturação do Header.
    
    * fix: Correção de espaços
    
    * fix: Correção de conflitos de mesclagem
    
    * fix: Correção do "Olá, Usuário"
    
    * fix: Exclusão do Link
    
    * fix: Correção de quebra de linhas
    
    * fix: Correção de quebra de linhas
    
    * fix: Faz a refaturação do Header.
    
    * fix: Correção de espaços
    
    * fix: Correção de conflitos de mesclagem
    
    * fix: Correção do "Olá, Usuário"
    
    * fix: Correção de quebra de linhas
    
    * fix: Correção de quebra de linhas
    
    * fix: Implementação do test
    
    * fix: Correção
    
    * fix: Faz a refaturação do Header.
    
    * fix: Correção de espaços
    
    * fix: Correção de conflitos de mesclagem
    
    * fix: Correção do "Olá, Usuário"
    
    * fix: Correção de quebra de linhas
    
    * fix: Correção de quebra de linhas
    
    * fix: Faz a refaturação do Header.
    
    * fix: Correção de espaços
    
    * fix: Correção de conflitos de mesclagem
    
    * fix: Correção do "Olá, Usuário"
    
    * fix: Exclusão do Link
    
    * fix: Correção de quebra de linhas
    
    * fix: Correção de quebra de linhas
    
    * fix: Implementação do test
    
    * fix: Correção
    
    * fix: Correção da refaturação do header
    
    * fix: Correção de aspas simples.
    
    * fix: Correção de caminhos.
    
    * fix: Correção de linhas.
    
    * fix: Correção de pipeline
    
    * fix: Muda Direcionamento ao Clicar no Logo
    
    * fix: Conserta Teste
    
    ---------
    
    Co-authored-by: Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -1,6 +1,6 @@
-import {render, screen} from '@testing-library/react'
-import {describe, expect, test} from 'vitest'
-import Header from './index';
+import { render, screen } from '@testing-library/react'
+import { describe, expect, test } from 'vitest'
+import Header from './Header';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('Testa o componente da Header', () => {
@@ -8,14 +8,12 @@ describe('Testa o componente da Header', () => {
   test('Deve verificar a renderização', () => {
     render(
       <BrowserRouter>
-        <Header/>
+        <Header />
       </BrowserRouter>)
-    const logo = screen.getByRole('logo')
-    const sobre = screen.getByText('O que é Bookclub?')
-    const contato = screen.getByText('Contato')
-    const administrador = screen.getByText('Administrador')
+    const logo = screen.getByRole('Logo');
+    const avatar = screen.getByRole('Avatar');
 
-    const componentes = [logo, sobre, contato, administrador]
+    const componentes = [logo, avatar];
 
     componentes.forEach((componente => {
       expect(componente).toBeVisible()
@@ -25,52 +23,13 @@ describe('Testa o componente da Header', () => {
   test('Testa Redirecionamento da Home', () => {
     render(
       <BrowserRouter>
-        <Header/>
+        <Header />
       </BrowserRouter>)
-    
-    const logo = screen.getByRole('logo')
+    const logo = screen.getByRole('Logo')
     expect(logo).toBeVisible()
 
     logo.click();
-    expect(window.location.pathname).toBe('/')
-  })
-
-  test('Testa Redirecionamento do Sobre', () => {
-    render(
-      <BrowserRouter>
-        <Header/>
-      </BrowserRouter>)
-    
-    const sobre = screen.getByText('O que é Bookclub?')
-    expect(sobre).toBeVisible()
-
-    sobre.click();
-    expect(window.location.pathname).toBe('/sobre')
-  })
-
-  test('Testa Redirecionamento do Contato', () => {
-    render(
-      <BrowserRouter>
-        <Header/>
-      </BrowserRouter>)
-    
-    const contato = screen.getByText('Contato')
-    expect(contato).toBeVisible()
-
-    contato.click();
-    expect(window.location.pathname).toBe('/contato')
-  })
-
-  test('Testa Redirecionamento do Administrador', () => {
-    render(
-      <BrowserRouter>
-        <Header/>
-      </BrowserRouter>)
-    
-    const administrador = screen.getByText('Administrador')
-    expect(administrador).toBeVisible()
-
-    administrador.click();
     expect(window.location.pathname).toBe('/adm')
   })
+ 
 });
