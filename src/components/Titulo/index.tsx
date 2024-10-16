8a5b237db6fda478cbdba359cba6468d22f2a532
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-10-16 09:18:44 -0300

    fix: Conserta erros de Lint

--- src/components/Titulo/index.tsx
+++ src/components/Titulo/index.tsx
@@ -1,17 +1,17 @@
-import styles from "./titulo.module.css"
+import styles from './titulo.module.css'
 interface Info {
     titulo: string,
 }
 
 
 const Titulo = ({titulo}: Info) => {
-    return (
-        <>
-            <h1 className={styles.titulo}>
-                {titulo}
-            </h1>
-        </>
-    )
+  return (
+    <>
+      <h1 className={styles.titulo}>
+        {titulo}
+      </h1>
+    </>
+  )
 }
 
 export default Titulo;
