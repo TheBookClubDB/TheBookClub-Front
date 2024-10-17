49b0d4c26efe5d6fa4caa20736f03318f2884f5f
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-10-17 10:22:52 -0300

    fix: Arruma erros de Lint

--- src/pages/NaoEncontrada/index.tsx
+++ src/pages/NaoEncontrada/index.tsx
@@ -1,25 +1,25 @@
 import styles from './naoEncontrada.module.css'
-import Botao from "../../components/botao/Botao"
-import { Box } from "@mui/material"
-import { useNavigate } from "react-router-dom"
+import Botao from '../../components/botao/Botao'
+import { Box } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const NaoEncontrada = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const redirecionamento = () => {
-        navigate('/')
-    }
+  const redirecionamento = () => {
+    navigate('/')
+  }
 
-    return (
-        <Box className={styles.pageBody}>
-            <h3>Página não encontrada!</h3>
-            <Botao
-                onClick={redirecionamento}
-                texto="Voltar para Home"
-                variante='outlined'
-            />
-        </Box>
-    )
+  return (
+    <Box className={styles.pageBody}>
+      <h3>Página não encontrada!</h3>
+      <Botao
+        onClick={redirecionamento}
+        texto="Voltar para Home"
+        variante='outlined'
+      />
+    </Box>
+  )
 }
 
 export default NaoEncontrada;
