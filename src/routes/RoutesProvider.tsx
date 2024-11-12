699cef50fd8636f59b6316f6ca81007d8bb48067
Tomsilva69 <evertondosantos69@gmail.com>
2024-11-12 14:56:08 -0300

    fix: Correção de conflitos

--- src/routes/RoutesProvider.tsx
+++ src/routes/RoutesProvider.tsx
@@ -4,60 +4,59 @@ import Sidebar from '../components/Sidebar';
 import NaoEncontrada from '../pages/NaoEncontrada';
 import Tabela from '../components/Tabela/table';
 
+function RoutesProvider() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Sidebar pagina='' />,
+    },
+    {
+      path: '/devolver',
+      element: <div>Devolver</div>, // substituído <text> por <div>
+    },
+    {
+      path: '/alugar',
+      element: <div>Alugar</div>, // substituído <text> por <div>
+    },
+    {
+      path: '/historico',
+      element: <div>Histórico</div>, // substituído <text> por <div>
+    },
+    {
+      path: '/adm',
+      element: <div>Administrador</div>,
+    },
+    {
+      path: '/criar-autor',
+      element: <FormAutor />,
+    },
+    {
+      path: '/criar-locatario',
+      element: <div>Criar Locatário</div>, // substituído <text> por <div>
+    },
+    {
+      path: '/criar-livro',
+      element: <div>Criar Livro</div>, // substituído <text> por <div>
+    },
+    {
+      path: '/geren-autor',
+      element: <Tabela />,
+    },
+    {
+      path: '/geren-locatario',
+      element: <div>Gerenciar Locatários</div>, // substituído <text> por <div>
+    },
+    {
+      path: '/geren-livro',
+      element: <div>Gerenciar Livros</div>, // substituído <text> por <div>
+    },
+    {
+      path: '*',
+      element: <NaoEncontrada />,
+    },
+  ]);
 
-function RoutesProvider(){
-  const router = createBrowserRouter([{
-    path:'/',
-    element:<Sidebar pagina=''/>
-  },
-  {
-    path:'/devolver',
-    element:<text>Devolver</text>
-  },
-  {
-    path:'/alugar',
-    element:<text>Alugar</text>
-  },
-  {
-    path:'/historico',
-    element:<text>Histórico</text>
-  },
-  {
-    path:'/adm',
-    element:<div>Administrador</div>
-  },
-  {
-    path:'/criar-autor',
-    element:<FormAutor/>
-  },
-  {
-    path:'/criar-locatario',
-    element:<text>Criar Locatário</text>
-  },
-  {
-    path:'/criar-livro',
-    element:<text>Criar Livro</text>
-  },
-  {
-    path:'/geren-autor',
-    element:<Tabela/>
-  },
-  {
-    path:'/geren-locatario',
-    element:<text>Gerenciar Locatários</text>
-  },
-  {
-    path:'/geren-livro',
-    element:<text>Gerenciar Livros</text>
-  },
-  {
-    path:'*',
-    element:<NaoEncontrada/>
-  }])
-
-  return(
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default RoutesProvider;
